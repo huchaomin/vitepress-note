40ad4192dcb4f381eb3a1d498f17633a856beb5d
huchaomin <iisa_peter@163.com>
2024-10-15 23:54:53 +0800

    优化代码

--- .vitepress/theme/hideWaiting.ts
+++ .vitepress/theme/hideWaiting.ts
@@ -1,15 +1,17 @@
 /*
  * @Author       : peter peter@qingcongai.com
  * @Date         : 2024-10-15 11:48:52
- * @LastEditors  : peter peter@qingcongai.com
- * @LastEditTime : 2024-10-15 14:04:20
+ * @LastEditors  : huchaomin iisa_peter@163.com
+ * @LastEditTime : 2024-10-15 20:28:01
  * @Description  :
  */
 
-export default () => {
-  const waiting = document.querySelector('#waiting') as HTMLElement
-  waiting.addEventListener('animationend', () => {
-    waiting.remove()
+if (!import.meta.env.SSR && import.meta.env.PROD) {
+  window.addEventListener('load', () => {
+    const waiting = document.querySelector('#waiting') as HTMLElement
+    waiting.addEventListener('animationend', () => {
+      waiting.remove()
+    })
+    waiting.style.opacity = '0'
   })
-  waiting.style.opacity = '0'
 }
--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -2,21 +2,16 @@
  * @Author       : huchaomin
  peter@qingcongai.com
  * @Date         : 2024-07-23 17:47:23
- * @LastEditors  : peter peter@qingcongai.com
- * @LastEditTime : 2024-10-15 14:37:04
+ * @LastEditors  : huchaomin iisa_peter@163.com
+ * @LastEditTime : 2024-10-15 20:28:05
  * @Description  :
  */
 // https://vitepress.dev/guide/custom-theme
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme-without-fonts'
 import '@/assets/css/index.css'
-import hideWaiting from './hideWaiting'
+import './hideWaiting'
 
-if (!import.meta.env.SSR && import.meta.env.PROD) {
-  window.addEventListener('load', () => {
-    hideWaiting()
-  })
-}
 export default {
   // Vue 应用实例, 路由实例, 站点数据
   enhanceApp({ app, router, siteData }) {
