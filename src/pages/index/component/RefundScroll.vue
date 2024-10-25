5d9085bba1f31939d1b65a27b74a30d6ddd3c35e
peter <peter@qingcongai.com>
2024-10-25 18:16:03 +0800

    开始别的组件

---
+++ src/pages/index/component/RefundScroll.vue
@@ -0,0 +1,12 @@
+<!--
+ * @Author       : peter peter@qingcongai.com
+ * @Date         : 2024-10-25 18:15:43
+ * @LastEditors  : peter peter@qingcongai.com
+ * @LastEditTime : 2024-10-25 18:15:51
+ * @Description  :
+-->
+<template>
+  <div>
+
+  </div>
+</template>
