5b00ea005222967c55cd3e1307815ee32e55553a
huchaomin <iisa_peter@163.com>
2024-11-03 22:59:33 +0800

    添加大屏页头

--- src/pages/index/component/page1/Index.vue
+++ src/pages/index/component/page1/Index.vue
@@ -1,17 +1,19 @@
 <!--
  * @Author       : huchaomin iisa_peter@163.com
  * @Date         : 2024-10-26 21:54:57
- * @LastEditors  : peter peter@qingcongai.com
- * @LastEditTime : 2024-11-01 17:46:52
+ * @LastEditors  : huchaomin iisa_peter@163.com
+ * @LastEditTime : 2024-11-03 21:57:58
  * @Description  :
 -->
 <script setup lang="ts">
 import RefundScroll from './refundScroll/Index.vue'
 import RefundUser from './refundUser/Index.vue'
+import Header from './header/Index.vue'
 </script>
 
 <template>
   <div class="page1_wrapper absolute h-full w-full">
+    <Header></Header>
     <RefundScroll></RefundScroll>
     <RefundUser></RefundUser>
   </div>
---
+++ src/pages/index/component/page1/header/Index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import headerSvg from '@/pages/index/assets/svg/header.svg?raw'
+</script>
+
+<template>
+  <div v-html="headerSvg">
+
+  </div>
+</template>
