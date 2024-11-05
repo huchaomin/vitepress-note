628a8944b6a61287d3c11c5736f4fdae9b8efaee
peter <peter@qingcongai.com>
2024-11-05 15:17:16 +0800

    添加累计回款

--- src/pages/index/component/page1/Index.vue
+++ src/pages/index/component/page1/Index.vue
@@ -2,7 +2,7 @@
  * @Author       : huchaomin iisa_peter@163.com
  * @Date         : 2024-10-26 21:54:57
  * @LastEditors  : peter peter@qingcongai.com
- * @LastEditTime : 2024-11-05 14:02:08
+ * @LastEditTime : 2024-11-05 14:43:31
  * @Description  :
 -->
 <script setup lang="ts">
@@ -12,6 +12,7 @@ import Header from './header/Index.vue'
 import AssetDistribution from './assetDistribution/Index.vue'
 import RefundType from './refundType/Index.vue'
 import RefundTrend from './refundTrend/Index.vue'
+import RefundTotal from './refundTotal/Index.vue'
 </script>
 
 <template>
@@ -22,6 +23,7 @@ import RefundTrend from './refundTrend/Index.vue'
     <RefundUser></RefundUser>
     <RefundType></RefundType>
     <RefundTrend></RefundTrend>
+    <RefundTotal></RefundTotal>
   </div>
 </template>
 
---
+++ src/pages/index/component/page1/refundTotal/Index.vue
@@ -0,0 +1,37 @@
+<!--
+ * @Author       : peter peter@qingcongai.com
+ * @Date         : 2024-11-05 14:42:15
+ * @LastEditors  : peter peter@qingcongai.com
+ * @LastEditTime : 2024-11-05 15:02:52
+ * @Description  :
+-->
+<script setup lang="ts">
+import { DotLottieVue } from '@lottiefiles/dotlottie-vue'
+import assets_gather from '@/pages/index/assets/json/lottie/assets_gather.json?url'
+
+const key = useVwToPx(1)
+
+</script>
+
+<template>
+  <div class="refund_total_wrapper absolute flex align-center">
+    <DotLottieVue :key="key" class="icon absolute" autoplay loop :src="assets_gather"></DotLottieVue>
+    累计回款：923472377823478 元
+  </div>
+</template>
+
+<style scoped>
+  .refund_total_wrapper {
+    top: 8vw;
+    left: 50%;
+    font-size: 2rem;
+    transform: translateX(-50%);
+  }
+
+  .icon {
+    top: 40%;
+    left: -2.5vw;
+    width: 12vw;
+    transform: translate(-50%, -50%);
+  }
+</style>
