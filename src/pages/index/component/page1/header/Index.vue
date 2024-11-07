0d2aef2c56c65c083193c9c3927aa23dd03508df
peter <peter@qingcongai.com>
2024-11-07 09:50:45 +0800

    调整标题文字

--- src/pages/index/component/page1/header/Index.vue
+++ src/pages/index/component/page1/header/Index.vue
@@ -1,9 +1,14 @@
+<!--
+ * @Author       : peter peter@qingcongai.com
+ * @Date         : 2024-11-04 09:20:35
+ * @LastEditors  : peter peter@qingcongai.com
+ * @LastEditTime : 2024-11-07 09:31:09
+ * @Description  :
+-->
 <script setup lang="ts">
 import headerSvg from '@/pages/index/assets/svg/header.svg?raw'
 </script>
 
 <template>
-  <div v-html="headerSvg">
-
-  </div>
+  <div v-html="headerSvg"></div>
 </template>
--- src/pages/index/index.ts
+++ src/pages/index/index.ts
@@ -1,8 +1,8 @@
 /*
  * @Author       : peter peter@qingcongai.com
  * @Date         : 2024-10-21 14:24:06
- * @LastEditors  : huchaomin iisa_peter@163.com
- * @LastEditTime : 2024-11-02 22:56:21
+ * @LastEditors  : peter peter@qingcongai.com
+ * @LastEditTime : 2024-11-07 09:47:04
  * @Description  :
  */
 import { Fog, Color, Group } from 'three'
@@ -34,6 +34,7 @@ export default class CanvasRender extends ThreeCore {
     config?: ConstructorParameters<typeof ThreeCore>[1],
   ) {
     super(canvas, config)
+    this.setAxesHelper()
     // 中心坐标
     this.depth = 5
     this.scene.fog = new Fog(0x011024, 1, 500)
