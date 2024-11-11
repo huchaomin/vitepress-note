ae556bc6b7bda36a0e8f3849282b20e597b000ae
peter <peter@qingcongai.com>
2024-11-11 18:03:47 +0800

    添加动画

--- src/pages/index/component/page1/Index.vue
+++ src/pages/index/component/page1/Index.vue
@@ -2,10 +2,11 @@
  * @Author       : huchaomin iisa_peter@163.com
  * @Date         : 2024-10-26 21:54:57
  * @LastEditors  : peter peter@qingcongai.com
- * @LastEditTime : 2024-11-07 16:08:06
+ * @LastEditTime : 2024-11-11 18:02:45
  * @Description  :
 -->
 <script setup lang="ts">
+import gsap from 'gsap'
 import RefundScroll from './refundScroll/Index.vue'
 import RefundUser from './refundUser/Index.vue'
 import AssetDistribution from './assetDistribution/Index.vue'
@@ -13,22 +14,76 @@ import RefundType from './refundType/Index.vue'
 import RefundTrend from './refundTrend/Index.vue'
 import RefundTotal from './refundTotal/Index.vue'
 import AssetSize from './assetSize/Index.vue'
+
+const refundScrollRef = ref<InstanceType<typeof RefundScroll> | null>(null)
+const refundUserRef = ref<InstanceType<typeof RefundUser> | null>(null)
+const assetDistributionRef = ref<InstanceType<typeof AssetDistribution> | null>(null)
+const refundTypeRef = ref<InstanceType<typeof RefundType> | null>(null)
+const refundTrendRef = ref<InstanceType<typeof RefundTrend> | null>(null)
+const refundTotalRef = ref<InstanceType<typeof RefundTotal> | null>(null)
+const assetSizeRef = ref<InstanceType<typeof AssetSize> | null>(null)
+
+const isMounted = useMounted()
+
+watch(isMounted, (val) => {
+  if (val) {
+    const tl = gsap.timeline()
+    tl.addLabel('top_left', 0)
+    tl.addLabel('top_right', 0.5)
+    tl.addLabel('bottom_left', 1)
+    tl.addLabel('bottom_right', 1.5)
+    tl.add(
+      gsap.to(assetDistributionRef.value!.$el, {
+        duration: 2,
+        ease: 'circ.out',
+        translateX: 0,
+      }),
+      'top_left',
+    )
+    tl.add(
+      gsap.to(refundTypeRef.value!.$el, {
+        duration: 2,
+        ease: 'circ.out',
+        translateX: 0,
+      }),
+      'top_right',
+    )
+    tl.add(
+      gsap.to(refundUserRef.value!.$el, {
+        duration: 2,
+        ease: 'circ.out',
+        translateX: 0,
+      }),
+      'bottom_left',
+    )
+    tl.add(
+      gsap.to(refundTrendRef.value!.$el, {
+        duration: 2,
+        ease: 'circ.out',
+        translateX: 0,
+      }),
+      'bottom_right',
+    )
+  }
+})
 </script>
 
 <template>
   <div class="page1_wrapper absolute h-full w-full">
-    <RefundScroll></RefundScroll>
-    <AssetDistribution></AssetDistribution>
-    <RefundUser></RefundUser>
-    <RefundType></RefundType>
-    <RefundTrend></RefundTrend>
-    <RefundTotal></RefundTotal>
-    <AssetSize></AssetSize>
+    <RefundScroll ref="refundScrollRef"></RefundScroll>
+    <AssetDistribution ref="assetDistributionRef"></AssetDistribution>
+    <RefundUser ref="refundUserRef"></RefundUser>
+    <RefundType ref="refundTypeRef"></RefundType>
+    <RefundTrend ref="refundTrendRef"></RefundTrend>
+    <RefundTotal ref="refundTotalRef"></RefundTotal>
+    <AssetSize ref="assetSizeRef"></AssetSize>
   </div>
 </template>
 
 <style scoped>
 .page1_wrapper {
+  --x-padding: 1.8vw;
+
   top: 0;
   left: 0;
   z-index: 1;
@@ -43,13 +98,18 @@ import AssetSize from './assetSize/Index.vue'
 
   .refund_user_wrapper,
   .asset_distribution_wrapper {
-    left: 1.8vw;
+    left: var(--x-padding);
+    transform: translateX(calc(-100% - var(--x-padding)));
   }
 
   .refund_type_wrapper,
-  .refund_trend_wrapper,
+  .refund_trend_wrapper {
+    right: var(--x-padding);
+    transform: translateX(calc(100% + var(--x-padding)));
+  }
+
   .asset_size_wrapper {
-    right: 1.8vw;
+    right: var(--x-padding);
   }
 
   .refund_user_wrapper,
