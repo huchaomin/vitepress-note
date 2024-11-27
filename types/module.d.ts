d5c764d54768bbff8b5ed9b7e3e7de9bc1a10353
huchaomin <iisa_peter@163.com>
2024-11-28 00:05:33 +0800

    解决ts 不识别vue文件，而报错的问题

---
+++ types/module.d.ts
@@ -0,0 +1,16 @@
+/*
+ * @Author       : huchaomin iisa_peter@163.com
+ * @Date         : 2024-11-27 23:40:44
+ * @LastEditors  : huchaomin iisa_peter@163.com
+ * @LastEditTime : 2024-11-28 00:03:17
+ * @Description  :
+ */
+// https://segmentfault.com/q/1010000042015279
+// https://juejin.cn/post/7008710181769084964
+// https://juejin.cn/post/7229877486170439740
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue'
+
+  const component: DefineComponent
+  export default component
+}
