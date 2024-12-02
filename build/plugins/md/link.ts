29a04e9c2a44928623f5a89111f193c7ae1fe36c
peter <peter@qingcongai.com>
2024-12-02 10:58:57 +0800

    重构 renderer.rules.link_open

--- build/plugins/md/link.ts
+++ build/plugins/md/link.ts
@@ -2,27 +2,18 @@
  * @Author       : peter peter@qingcongai.com
  * @Date         : 2024-11-23 10:49:38
  * @LastEditors  : peter peter@qingcongai.com
- * @LastEditTime : 2024-12-02 09:52:56
+ * @LastEditTime : 2024-12-02 10:58:17
  * @Description  :
  */
 import type { MarkdownIt } from './index.ts'
 
 export default (md: MarkdownIt) => {
-  md.renderer.rules.link_open = (tokens, idx, options) => {
-    const { tag } = tokens[idx]
-    // @ts-expect-error vitepress 类型没有写的那么全
-    const externalLinks = (options.externalLinks ?? {
-      rel: 'noopener noreferrer',
-      target: '_blank',
-    }) as Record<string, string>
-    Object.keys(externalLinks).forEach((key) => {
-      tokens[idx].attrSet(key, externalLinks[key])
-    })
-    const attrsStr = md.renderer.renderAttrs(tokens[idx])
-    return `<n-${tag} ${attrsStr}><n-gradient-text type="primary">`
+  const defaultRender = md.renderer.rules.link_open
+  md.renderer.rules.link_open = (...arg) => {
+    // 这里直接修改 token 的 type 不生效
+    return `${defaultRender!(...arg).replace('<a', '<n-a')}<n-gradient-text type="primary">`
   }
-  md.renderer.rules.link_close = (tokens, idx) => {
-    const { tag } = tokens[idx]
-    return `</n-gradient-text></n-${tag}>`
+  md.renderer.rules.link_close = () => {
+    return `</n-gradient-text></n-a>`
   }
 }
