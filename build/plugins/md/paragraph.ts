441d36ed8da278d1b1c8319cee24fe09e4bb9dd7
peter <peter@qingcongai.com>
2024-12-02 11:19:15 +0800

    小修改

--- build/plugins/md/paragraph.ts
+++ build/plugins/md/paragraph.ts
@@ -1,19 +1,17 @@
 /*
  * @Author       : peter peter@qingcongai.com
  * @Date         : 2024-11-23 10:49:38
- * @LastEditors  : huchaomin iisa_peter@163.com
- * @LastEditTime : 2024-11-30 22:32:54
+ * @LastEditors  : peter peter@qingcongai.com
+ * @LastEditTime : 2024-12-02 11:13:00
  * @Description  :
  */
 import type { MarkdownIt } from './index.ts'
 
 export default (md: MarkdownIt) => {
-  md.renderer.rules.paragraph_open = (tokens, idx) => {
-    const { tag } = tokens[idx]
-    return `<n-${tag}>`
+  md.renderer.rules.paragraph_open = () => {
+    return `<n-p>`
   }
-  md.renderer.rules.paragraph_close = (tokens, idx) => {
-    const { tag } = tokens[idx]
-    return `</n-${tag}>`
+  md.renderer.rules.paragraph_close = () => {
+    return `</n-p>`
   }
 }
