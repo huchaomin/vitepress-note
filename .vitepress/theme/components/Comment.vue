54d75b84ca4f45656509cd1da5c0bcb824c749a1
huchaomin <iisa_peter@163.com>
2024-12-18 17:31:43 +0800

    调整 评论样式

--- .vitepress/theme/components/Comment.vue
+++ .vitepress/theme/components/Comment.vue
@@ -2,7 +2,7 @@
  * @Author       : huchaomin iisa_peter@163.com
  * @Date         : 2024-12-18 09:56:13
  * @LastEditors  : huchaomin iisa_peter@163.com
- * @LastEditTime : 2024-12-18 17:25:24
+ * @LastEditTime : 2024-12-18 17:31:27
  * @Description  :
 -->
 <script setup lang="ts">
@@ -15,24 +15,26 @@ const isProd = import.meta.env.PROD
 </script>
 
 <template>
-  <ClientOnly>
-    <Giscus
-      v-if="frontmatter.uuid && isProd"
-      :key="frontmatter.uuid"
-      repo="huchaomin/vitepress-note"
-      repo-id="R_kgDOMZ-wiw"
-      category="Announcements"
-      category-id="DIC_kwDOMZ-wi84ClUQt"
-      mapping="og:title"
-      strict="1"
-      reactions-enabled="1"
-      emit-metadata="1"
-      input-position="top"
-      theme="preferred_color_scheme"
-      lang="zh-CN"
-      loading="lazy"
-      crossorigin="anonymous"
-      async
-    ></Giscus>
-  </ClientOnly>
+  <div>
+    <ClientOnly>
+      <Giscus
+        v-if="frontmatter.uuid && isProd"
+        :key="frontmatter.uuid"
+        repo="huchaomin/vitepress-note"
+        repo-id="R_kgDOMZ-wiw"
+        category="Announcements"
+        category-id="DIC_kwDOMZ-wi84ClUQt"
+        mapping="og:title"
+        strict="1"
+        reactions-enabled="1"
+        emit-metadata="1"
+        input-position="top"
+        theme="preferred_color_scheme"
+        lang="zh-CN"
+        loading="lazy"
+        crossorigin="anonymous"
+        async
+      ></Giscus>
+    </ClientOnly>
+  </div>
 </template>
