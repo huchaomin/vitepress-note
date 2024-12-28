95ac928f731791ff23ca0a430a3c676f96bec1b0
huchaomin <iisa_peter@163.com>
2024-12-28 23:32:27 +0800

    解决 stylelint 不生效的问题

--- src/pages/tools/vscode.md
+++ src/pages/tools/vscode.md
@@ -3,8 +3,8 @@ uuid         : 5187e96b-78c1-4302-ac44-311528a12cbb
 order        : 2
 author       : peter peter@qingcongai.com
 date         : 2024-11-25 14:23:03
-lastEditTime : 2024-12-10 10:55:21
-lastEditors  : peter peter@qingcongai.com
+lastEditTime : 2024-12-28 23:30:37
+lastEditors  : huchaomin iisa_peter@163.com
 description  :
 ---
 # vscode 使用经验
@@ -14,3 +14,28 @@ description  :
 ### TSServer exited. Code: null. Signal: SIGTERM
 
 [Reload with Extensions Disabled](https://github.com/microsoft/vscode/issues/191441)
+
+### stylelint 只有在编辑器第一次打开时生效，改动文件之后不生效
+
+初步实验是 stylelint 与 Tailwind CSS IntelliSense 插件冲突，关闭 Tailwind CSS IntelliSense 插件后 stylelint 生效。
+
+如果想要同时使用两个插件，可以在 settings.json 中配置：
+
+```json
+{
+  "files.associations": {
+    "*.css": "tailwindcss"
+  },
+  // ...
+  "stylelint.validate": [
+  // ...
+    "tailwindcss"
+  ],
+  "stylelint.snippet": [
+  // ...
+    "tailwindcss"
+  ]
+}
+```
+
+经过测试，没问题
--- types/auto-imports.d.ts
+++ types/auto-imports.d.ts
@@ -239,11 +239,13 @@ declare global {
   const usePreferredDark: typeof import('@vueuse/core')['usePreferredDark']
   const usePreferredLanguages: typeof import('@vueuse/core')['usePreferredLanguages']
   const usePreferredReducedMotion: typeof import('@vueuse/core')['usePreferredReducedMotion']
+  const usePreferredReducedTransparency: typeof import('@vueuse/core')['usePreferredReducedTransparency']
   const usePrevious: typeof import('@vueuse/core')['usePrevious']
   const useRafFn: typeof import('@vueuse/core')['useRafFn']
   const useRefHistory: typeof import('@vueuse/core')['useRefHistory']
   const useRequest: typeof import('alova/client')['useRequest']
   const useResizeObserver: typeof import('@vueuse/core')['useResizeObserver']
+  const useSSRWidth: typeof import('@vueuse/core')['useSSRWidth']
   const useScreenOrientation: typeof import('@vueuse/core')['useScreenOrientation']
   const useScreenSafeArea: typeof import('@vueuse/core')['useScreenSafeArea']
   const useScriptTag: typeof import('@vueuse/core')['useScriptTag']
@@ -565,11 +567,13 @@ declare module 'vue' {
     readonly usePreferredDark: UnwrapRef<typeof import('@vueuse/core')['usePreferredDark']>
     readonly usePreferredLanguages: UnwrapRef<typeof import('@vueuse/core')['usePreferredLanguages']>
     readonly usePreferredReducedMotion: UnwrapRef<typeof import('@vueuse/core')['usePreferredReducedMotion']>
+    readonly usePreferredReducedTransparency: UnwrapRef<typeof import('@vueuse/core')['usePreferredReducedTransparency']>
     readonly usePrevious: UnwrapRef<typeof import('@vueuse/core')['usePrevious']>
     readonly useRafFn: UnwrapRef<typeof import('@vueuse/core')['useRafFn']>
     readonly useRefHistory: UnwrapRef<typeof import('@vueuse/core')['useRefHistory']>
     readonly useRequest: UnwrapRef<typeof import('alova/client')['useRequest']>
     readonly useResizeObserver: UnwrapRef<typeof import('@vueuse/core')['useResizeObserver']>
+    readonly useSSRWidth: UnwrapRef<typeof import('@vueuse/core')['useSSRWidth']>
     readonly useScreenOrientation: UnwrapRef<typeof import('@vueuse/core')['useScreenOrientation']>
     readonly useScreenSafeArea: UnwrapRef<typeof import('@vueuse/core')['useScreenSafeArea']>
     readonly useScriptTag: UnwrapRef<typeof import('@vueuse/core')['useScriptTag']>
