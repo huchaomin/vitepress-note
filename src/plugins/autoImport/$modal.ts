c4f31f7b6243ebf77de26dc8226b15da88f1aa4f
huchaomin <iisa_peter@163.com>
2025-01-06 09:23:30 +0800

    $modal

--- src/plugins/autoImport/$modal.ts
+++ src/plugins/autoImport/$modal.ts
@@ -2,14 +2,20 @@
  * @Author       : peter peter@qingcongai.com
  * @Date         : 2024-11-11 09:24:19
  * @LastEditors  : huchaomin iisa_peter@163.com
- * @LastEditTime : 2024-12-31 09:13:05
+ * @LastEditTime : 2025-01-03 17:45:03
  * @Description  :
  */
-import type { ModalOptions } from 'naive-ui'
+import type { ModalApi } from 'naive-ui'
 
 import { useModal } from '@/plugins/naive-ui/discreteApi'
 
-export default (config: ModalOptions) => {
+type ModalApiCreate = ModalApi['create']
+
+interface ModalApiDestroyAll {
+  destroyAll: () => void
+}
+
+const create: ModalApiCreate = (config) => {
   return useModal().create({
     autoFocus: false,
     closeOnEsc: false,
@@ -22,3 +28,9 @@ export default (config: ModalOptions) => {
     ...(config ?? {}),
   })
 }
+
+;(create as unknown as ModalApiDestroyAll).destroyAll = () => {
+  return useModal().destroyAll()
+}
+
+export default create as ModalApiCreate & ModalApiDestroyAll
