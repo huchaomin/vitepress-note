395f3295d5f983bc9f97b225687e3e2ebb77a234
huchaomin <iisa_peter@163.com>
2025-01-16 15:40:09 +0800

    $loading 重构

--- src/plugins/autoImport/$loading.ts
+++ src/plugins/autoImport/$loading.ts
@@ -1,19 +1,91 @@
 /*
  * @Author       : huchaomin iisa_peter@163.com
  * @Date         : 2023-11-05 11:29:13
- * @LastEditors  : peter peter@qingcongai.com
- * @LastEditTime : 2024-11-08 14:15:35
+ * @LastEditors  : huchaomin iisa_peter@163.com
+ * @LastEditTime : 2025-01-16 15:37:49
  * @Description  :
  */
-export default {
-  hide() {
-    const commonStore = useCommonStore(piniaInstance)
-    setTimeout(() => {
-      commonStore.loadingCount -= 1
-    }, 300)
-  },
-  show() {
-    const commonStore = useCommonStore(piniaInstance)
-    commonStore.loadingCount += 1
-  },
+import type { LoadingBarApi, MessageReactive, ModalReactive } from 'naive-ui'
+import type { VNodeChild } from 'vue'
+
+import { useLoadingBar } from '@/plugins/naive-ui/discreteApi'
+import { inBrowser } from 'vitepress'
+
+interface LoadingShowOptions {
+  content?: (() => VNodeChild) | string
+  lockScreen?: boolean
 }
+
+class LoadingService {
+  private static content: LoadingShowOptions['content']
+  private static loadingCount = 0
+  private static lockScreen: LoadingShowOptions['lockScreen'] = false
+  private static message: MessageReactive | null = null
+  private static modal: ModalReactive | null = null
+  private readonly loading: LoadingBarApi
+
+  constructor() {
+    this.loading = useLoadingBar()
+  }
+
+  private static updateLoading(instance: LoadingService, count: number) {
+    if (count > 0) {
+      if (this.loadingCount === 0) {
+        instance.loading.start()
+      }
+      if (this.content !== undefined) {
+        if (this.message === null) {
+          this.message = $msg.loading(this.content, {
+            duration: 0,
+          })
+        }
+        this.message.content = this.content
+      }
+      if (this.lockScreen) {
+        if (this.modal === null) {
+          this.modal = $modal({
+            class: 'loading_modal',
+            style: {
+              display: 'none',
+            },
+            trapFocus: false,
+          })
+        }
+      }
+    } else {
+      instance.loading.finish()
+      if (this.message !== null) {
+        this.message.destroy()
+        this.message = null
+      }
+      if (this.modal !== null) {
+        this.modal.destroy()
+        this.modal = null
+      }
+      this.content = undefined
+      this.lockScreen = false
+    }
+    this.loadingCount = count
+  }
+
+  hide() {
+    if (!inBrowser) {
+      return
+    }
+    // 等待动画结束
+    setTimeout(() => {
+      LoadingService.updateLoading(this, LoadingService.loadingCount - 1)
+    }, 300)
+  }
+
+  show(options: LoadingShowOptions = {}) {
+    if (!inBrowser) {
+      return
+    }
+    LoadingService.content = options.content ?? LoadingService.content
+    LoadingService.lockScreen = options.lockScreen ?? LoadingService.lockScreen
+    LoadingService.updateLoading(this, LoadingService.loadingCount + 1)
+  }
+}
+
+export default new LoadingService()
--- src/store/modules/common.ts
+++ src/store/modules/common.ts
@@ -2,42 +2,17 @@
  * @Author       : huchaomin peter@qingcongai.com
  * @Date         : 2023-10-30 15:31:12
  * @LastEditors  : huchaomin iisa_peter@163.com
- * @LastEditTime : 2024-12-16 14:14:50
+ * @LastEditTime : 2025-01-16 11:19:58
  * @Description  :
  */
-import type { LoadingBarApi } from 'naive-ui'
-
-import { useLoadingBar } from '@/plugins/naive-ui/discreteApi'
 import { inBrowser } from 'vitepress'
 
 export default defineStore('common', () => {
-  // 全局加载状态
-  const loading = ref(false)
-  const loadingCount = ref(0)
-  watch(loadingCount, (val) => {
-    loading.value = val > 0
-  })
-  let loadingBar: LoadingBarApi | null = null
-  watch(loading, (val) => {
-    if (!inBrowser) {
-      return
-    }
-    if (loadingBar === null) {
-      loadingBar = useLoadingBar()
-    }
-    if (val) {
-      loadingBar.start()
-    } else {
-      loadingBar.finish()
-    }
-  })
   const screenWidth = ref(0)
   const screenHeight = ref(0)
   const showLeftDrawer = ref(!(inBrowser && (isTablet.value || isMobile.value)))
 
   return {
-    loading,
-    loadingCount,
     screenHeight,
     screenWidth,
     showLeftDrawer,
