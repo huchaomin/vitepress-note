7fa74ddd2a99ed86cb237454a093877519acea83
huchaomin <iisa_peter@163.com>
2025-01-16 09:23:10 +0800

    $notify 重构完成

--- src/plugins/autoImport/$notify.ts
+++ src/plugins/autoImport/$notify.ts
@@ -1,76 +1,99 @@
 /*
- * @Author       : huchaomin peter@qingcongai.com
- * @Date         : 2023-07-20 09:00:28
+ * @Author       : huchaomin iisa_peter@163.com
+ * @Date         : 2025-01-15 18:28:27
  * @LastEditors  : huchaomin iisa_peter@163.com
- * @LastEditTime : 2025-01-03 23:04:50
+ * @LastEditTime : 2025-01-16 09:19:07
  * @Description  :
  */
 
-import type { NotificationOptions, NotificationReactive } from 'naive-ui'
+import type { NotificationApi, NotificationOptions, NotificationReactive } from 'naive-ui'
 
 import { useNotify } from '@/plugins/naive-ui/discreteApi'
 import { notificationPlacement, type PlacementType } from '@/plugins/naive-ui/providerProps'
 
-// key 暂时没有用到 $notify.create option.type 就为 default
-enum NotificationTypes {
-  default = 'create',
+enum NotificationCreateType {
+  create = 'create',
   error = 'error',
   info = 'info',
   success = 'success',
   warning = 'warning',
 }
 
-interface DestroyAll {
-  destroyAll: () => void
+interface CreateNotificationDestroyAllType {
+  destroyAll: NotificationApi['destroyAll']
 }
 
-type NotificationContentOptions = NotificationOptions['content']
-
-type NotificationTypesValues = `${NotificationTypes}`
-
-type Others = {
-  // 映射类型和函数类型不能写在一起
-  [value in NotificationTypes]: OthersType
-}
-
-type OthersType = (
-  content: NotificationContentOptions,
-  options?: Omit<NotificationOptions, 'content' | 'type'> & {
-    placement?: PlacementType
-  },
+type CreateNotificationFnType = (
+  content: NotificationOptions['content'],
+  options?: OptionsType,
 ) => NotificationReactive
 
-const create: OthersType = (...arg) => {
-  return useInject('success', ...arg)
+type CreateNotificationMappedType = {
+  [value in NotificationCreateType]: CreateNotificationFnType
 }
 
-function useInject(
-  type: NotificationTypesValues,
-  content: Parameters<OthersType>[0],
-  options?: Parameters<OthersType>[1],
-) {
-  const notification = useNotify()
-  const obj = {
-    ...(options ?? {}),
+/**
+ * @description: 不需要 type content 字段
+ */
+type OptionsType = Omit<NotificationOptions, 'content' | 'type'> & {
+  placement?: PlacementType
+}
+
+class NotificationService {
+  private readonly notification: NotificationApi
+
+  constructor() {
+    this.notification = useNotify()
   }
-  notificationPlacement.value = obj.placement ?? 'top-right'
-  delete obj.placement
-  return notification[type]({
-    content,
-    duration: 3000,
-    keepAliveOnHover: true,
-    title: '提示',
-    ...obj,
-  })
+
+  create(
+    type: `${NotificationCreateType}`,
+    content: Parameters<CreateNotificationFnType>[0],
+    options: Parameters<CreateNotificationFnType>[1],
+  ) {
+    const obj = {
+      ...(options ?? {}),
+    }
+    notificationPlacement.value = obj.placement ?? 'top-right'
+    delete obj.placement
+    return this.notification[type]({
+      content,
+      duration: 3000,
+      keepAliveOnHover: true,
+      title: '提示',
+      ...obj,
+    })
+  }
+
+  destroyAll() {
+    return this.notification.destroyAll()
+  }
 }
 
-Object.values(NotificationTypes).forEach((type) => {
-  ;(create as unknown as Others)[type] = (...arg) => {
-    return useInject(type, ...arg)
+const notificationService = new NotificationService()
+
+type CreateNotificationType = CreateNotificationDestroyAllType &
+  CreateNotificationFnType &
+  CreateNotificationMappedType
+
+function useNotifyCreate(
+  type: `${NotificationCreateType}`,
+  ...arg: Parameters<CreateNotificationFnType>
+) {
+  return notificationService.create(type, ...arg)
+}
+
+const createNotification: CreateNotificationType = function (...arg) {
+  return useNotifyCreate('success', ...arg)
+} as CreateNotificationType
+
+Object.values(NotificationCreateType).forEach((type) => {
+  createNotification[type] = (...arg) => {
+    return useNotifyCreate(type, ...arg)
   }
 })
-;(create as unknown as DestroyAll).destroyAll = () => {
-  const notification = useNotify()
-  return notification.destroyAll()
+createNotification.destroyAll = () => {
+  return notificationService.destroyAll()
 }
-export default create as DestroyAll & Others & OthersType
+
+export default createNotification
