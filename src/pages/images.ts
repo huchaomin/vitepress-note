6898b0a95b1dfef142075a6b9c064dee4f609051
huchaomin <iisa_peter@163.com>
2025-06-02 18:43:10 +0800

    生活

--- src/pages/images.ts
+++ src/pages/images.ts
@@ -1,10 +1,13 @@
 /*
  * @Author       : peter peter@qingcongai.com
  * @Date         : 2024-10-16 16:57:34
- * @LastEditors  : peter peter@qingcongai.com
- * @LastEditTime : 2024-12-05 10:13:35
+ * @LastEditors  : huchaomin iisa_peter@163.com
+ * @LastEditTime : 2025-06-02 13:09:49
  * @Description  :
  */
-export default import.meta.glob(['./**/*.png', './**/*.gif', '!./index/**/*.*'], {
-  import: 'default',
-}) as Record<string, () => Promise<string>>
+export default import.meta.glob(
+  ['./**/*.png', './**/*.jpg', './**/*.gif', '!./index/**/*.*', '!./public/**/*.*'],
+  {
+    import: 'default',
+  },
+) as Record<string, () => Promise<string>>
